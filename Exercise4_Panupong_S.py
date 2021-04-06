8860bf6fe33646a28d55610a703a3fad252cbdfa
kaitaklearn <panupong1661@gmail.com>
2021-04-07 00:59:14 +0700

    Exercise4_Panupong_S_addScore

--- Exercise4_Panupong_S.py
+++ Exercise4_Panupong_S.py
@@ -4,9 +4,9 @@ ITD_CS = 57
 CPPMG = 62.2
 
 print("----------YOUR SCORE----------")
-print("Foundation English              ", FDT_ENG)
-print("General Business                ", GNR_BSN)
-print("Introduction to Computer Systems",ITD_CS)
-print("Computer Programming            ",CPPMG)
+print("Foundation English              ",":", FDT_ENG)
+print("General Business                ",":", GNR_BSN)
+print("Introduction to Computer Systems",":",ITD_CS)
+print("Computer Programming            ",":",CPPMG)
 print("------------------------------")
 

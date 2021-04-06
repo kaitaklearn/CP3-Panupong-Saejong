46ff617deceac603761c6376d28baa4b645eb67a
kaitaklearn <82019173+kaitaklearn@users.noreply.github.com>
2021-04-06 22:59:09 +0700

    Add files via upload

---
+++ Exercise4_Panupong_S.py
@@ -0,0 +1,12 @@
+FDT_ENG = 68
+GNR_BSN = 65.8
+ITD_CS = 57
+CPPMG = 62.2
+
+print("----------YOUR SCORE----------")
+print("Foundation English              ", FDT_ENG)
+print("General Business                ", GNR_BSN)
+print("Introduction to Computer Systems",ITD_CS)
+print("Computer Programming            ",CPPMG)
+print("------------------------------")
+

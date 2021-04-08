9036e181ecd0a5440e4ef365e6910f931056be33
kaitaklearn <panupong1661@gmail.com>
2021-04-08 15:34:58 +0700

    Create Lecture50_Function_Panupong_S.py

---
+++ Lecture50_Function_Panupong_S.py
@@ -0,0 +1,14 @@
+def addNumber1(x,y):
+    print(x,"+",y,"=",x+y)
+def addNumber2(x,y):
+    print(x,"-",y,"=",x-y)
+def addNumber3(x,y):
+    print(x,"*",y,"=",x*y)
+def addNumber4(x,y):
+    print(x,"/",y,"=",x/y)
+x = int(input("No.1 "))
+y = int(input("No.2 "))
+addNumber1(x,y)
+addNumber2(x,y)
+addNumber3(x,y)
+addNumber4(x,y)
